945d9f71d685d63c65914b4a70324852d5c0a0c8
batu <alias.co>
2012-08-14 17:59:29 -0400

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
 
   get "static_pages/home"
 
   get "static_pages/help"
 
   get "static_pages/about"
+
+  resources :users
+  root to: 'static_pages#home'
+  match 'signup', to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
